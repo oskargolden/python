fccfe445e028eb6c1056500a57f1daf451d27870
oskar Golden <oskargolden@oskars-air.lan>
2022-09-03 12:24:18 -0500

    sept 6th assignment and gitignore

---
+++ class_Assignments/sept_6_ex.py
@@ -0,0 +1,18 @@
+name = 'oskar'
+
+print(name)
+
+age = 29
+
+print(age)
+
+major = input('please enter your major: ')
+
+print('Your major is: ', major)
+
+number = int(input('please enter a number: '))
+
+print('your number divide by 2: ',number / 2)
+
+
+print('Hi', name,', you are',age,' years old. Your major is', major, 'and your number is', [number],'and your number divide by two is: ', [number / 2])
