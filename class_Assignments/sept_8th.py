45ddb1eeb27e456a7facef2bc1612a216faccf1a
oskar Golden <oskargolden@oskars-MacBook-Air.local>
2022-09-08 16:33:08 -0500

    in class if-elif-else work

---
+++ class_Assignments/sept_8th.py
@@ -0,0 +1,20 @@
+print('\n WEEKDAY FINDER \n')
+
+userNum = int(input('Please enter a number between 1 and 7: '))
+
+if userNum == 1:
+    print('Today is Monday!')
+elif userNum == 2:
+    print('Today is Tuesday!')
+elif userNum == 3:
+    print('Today is Wednesday!')
+elif userNum ==4:
+    print('Today is Thursday!')
+elif userNum == 5:
+    print('Today is Friday!')
+elif userNum == 6:
+    print('Today is Saturday!')
+elif userNum == 7:
+     print('Today is Sunday!')
+else:
+    print('Error: There are only seven days in the week!')
