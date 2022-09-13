8d56e58e7d28839e1e447320967742407e7cf638
Oskar  Golden <85715187+oskargolden@users.noreply.github.com>
2022-09-13 09:17:11 -0500

    working through basic python loops

---
+++ while_keep_going.py
@@ -0,0 +1,10 @@
+keep_going = input("would you liek to start the task? If Yes Enter Y")
+
+while keep_going == 'Y':
+    sales = float(input('Enter the amount of sales: '))
+    comm_rate = float(input('Enter the commission rate: '))
+
+    commission = sales * comm_rate
+    print(f'The commission is ${commission:,.2f}.')
+    keep_going = input('Do you want to calculate another ' +'commission (Enter Y for yes): ')
+
