05fbfcae051fec4076906e966b8cc9887509210a
Oskar  Golden <85715187+oskargolden@users.noreply.github.com>
2022-09-28 13:21:52 -0500

    Start of Pygame learning

---
+++ Pygame/Basic_Window.py
@@ -0,0 +1,31 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+background = pygame.Surface((400, 300))
+
+surface = pygame.Surface((10,10))
+surface.fill((0,255,0))
+
+
+pos = [175, 125]
+
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                pos[0] += 20
+            if event.key == pygame.K_LEFT:
+                pos[0] -= 20
+            if event.key == pygame.K_UP:
+                pos[0] += 20
+            if event.key == pygame.K_DOWN:
+                pos[0] += 20
+    
+    screen.blit(surface, pos)
+    pygame.display.update()
